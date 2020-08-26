571b00cb9f98d48823da2ce735952ac790c350be
James Hall <1234824+james31415@users.noreply.github.com>
2020-08-25 20:49:04 -0400

    Don't block our followers

--- blocker.py
+++ blocker.py
@@ -1,9 +1,11 @@
 import twitter
 
-def block_followers(screen_name, cursor = -1):
+def block_followers(screen_name, my_followers, cursor = -1):
 	print("Trying to block {}'s followers".format(screen_name))
 
-	target_user = api.GetUser(screen_name = screen_name)
+	target_user = api.GetUser(
+		screen_name = screen_name,
+		include_entities = False)
 	if target_user.protected:
 		print("Can't get followers for {}. User is protected.".format(screen_name))
 		return
@@ -12,6 +14,8 @@ def block_followers(screen_name, cursor = -1):
 		print("Getting followers. Cursor: {}".format(cursor))
 		next_cursor, previous_cursor, followers = api.GetFollowersPaged(
 			screen_name = screen_name,
+			skip_status = True,
+			include_user_entities = False,
 			cursor = cursor)
 		print("Got {} followers".format(len(followers)))
 
@@ -19,16 +23,23 @@ def block_followers(screen_name, cursor = -1):
 			cursor = next_cursor
 
 		for follower in followers:
-			block_user(follower.screen_name)
+			block_user(follower.screen_name, my_followers)
 
 		if next_cursor == 0 or next_cursor == previous_cursor:
 			break
 
 	print("Blocking followers complete.")
 
-def block_user(screen_name):
+def block_user(screen_name, my_followers):
+	if screen_name in my_followers:
+		print("Didn't block {}: I follow them.".format(screen_name))
+		return
+
 	try:
-		blocked_user = api.CreateBlock(screen_name = screen_name)
+		blocked_user = api.CreateBlock(
+			screen_name = screen_name,
+			include_entities = False,
+			skip_status = True)
 		print("Blocked {} ({})".format(blocked_user.name, blocked_user.screen_name))
 	except twitter.error.TwitterError as e:
 		print("Block failed for user {}. ({})".format(screen_name, e))
@@ -54,8 +65,13 @@ if __name__ == "__main__":
 	cursor = -1
 	if len(sys.argv) == 3:
 		cursor = sys.argv[2]
-	block_followers(screen_name, cursor)
+
+	print("Getting my followers")
+	my_followers = [user.screen_name for user in api.GetFollowers(
+		skip_status = True,
+		include_user_entities = False)]
+	block_followers(screen_name, my_followers, cursor)
 
 	print("Trying to block {}.".format(screen_name))
-	block_user(screen_name)
+	block_user(screen_name, my_followers)
 	print("Done")
